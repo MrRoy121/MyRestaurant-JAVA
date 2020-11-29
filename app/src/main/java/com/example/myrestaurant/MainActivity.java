a1c775e163079219141698b87d5ed31df047dbfd
Nilashish Roy <nilashishroyjoy@gmail.com>
2020-11-29 15:10:00 +0600

    Added Main Activity

---
+++ app/src/main/java/com/example/myrestaurant/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.myrestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.b1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FoodListActivity.class);
+                i.putExtra("Select", "burger");
+                startActivity(i);
+            }
+        });
+        findViewById(R.id.b2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FoodListActivity.class);
+                i.putExtra("Select", "indian");
+                startActivity(i);
+            }
+        });
+        findViewById(R.id.b3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FoodListActivity.class);
+                i.putExtra("Select", "chinese");
+                startActivity(i);
+            }
+        });
+        findViewById(R.id.b4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FoodListActivity.class);
+                i.putExtra("Select", "drinks");
+                startActivity(i);
+            }
+        });
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.mybutton) {
+            startActivity(new Intent(getApplicationContext(), CartActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
